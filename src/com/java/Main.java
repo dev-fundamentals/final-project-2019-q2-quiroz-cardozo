9b1a3e4d509e2cdb7bded3a521918cd9eda7f9e8
tumael <tumael.suscripciones>
2019-07-10 22:25:48 -0400

    Added functionality for the Filter

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,12 +1,56 @@
 package com.java;
 
-import com.java.coffe.CoffeeMaker;
+import com.java.coffee.CoffeeMaker;
+
+import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 public class Main {
 
     public static void main(String[] args) {
         CoffeeMaker coffeeMaker = new CoffeeMaker();
-        coffeeMaker.putWaterGlasses(15);
-        coffeeMaker.pressBrewButton();
+//        coffeeMaker.putWaterGlasses(15);
+//        coffeeMaker.pressBrewButton();
+
+        int option = showMenu();
+
+        do {
+            switch (option) {
+                case 1:
+//                    coffeeMaker
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    exit(0);
+                    break;
+                default:
+                    System.out.println("Sorry, please enter valid Option");
+                    showMenu();
+                    break;
+            }
+        } while (option == 4);
+
+        System.out.println("Thank you. Good Bye.");
+    }
+
+    private static int showMenu() {
+        int option;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Coffee Maker Menu:");
+        System.out.println("-------------------------");
+        System.out.println("1. Place coffee grounds.");
+        System.out.println("2. Place water.");
+        System.out.println("3. Brew coffee.");
+        System.out.println("4. Quit");
+        System.out.println("-------------------------");
+        System.out.println("Enter your choice:");
+        option = keyboard.nextInt();
+
+        return option;
+
     }
 }
--- src/com/java/coffee/Filter.java
+++ src/com/java/coffee/Filter.java
@@ -1,4 +1,22 @@
-package com.java.coffe;
+package com.java.coffee;
 
 public class Filter {
+    private int coffeeGrounds;
+    private final int COFFEE_GROUNDS_LIMIT = 50; // is a number for determine the quantity.
+
+    public Filter() {
+        coffeeGrounds = 0;
+    }
+
+    public void setCoffeeGrounds(int quantityOfCoffeeGrounds) {
+        if (coffeeGrounds + quantityOfCoffeeGrounds > COFFEE_GROUNDS_LIMIT) {
+            throw new IllegalArgumentException("The capacity is only " + COFFEE_GROUNDS_LIMIT +  " coffee grounds.");
+        } else {
+            coffeeGrounds += quantityOfCoffeeGrounds;
+        }
+    }
+
+    public boolean containCoffeeGrounds() {
+        return coffeeGrounds != 0;
+    }
 }
