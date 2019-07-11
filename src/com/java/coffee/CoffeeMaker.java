417e8e8eb66b7c6dce420e501b9ba7a822d93bef
tumael <tumael.suscripciones@gmail.com>
2019-07-11 09:40:18 -0400

    Added a method to validate the coffee grounds

--- src/com/java/coffee/CoffeeMaker.java
+++ src/com/java/coffee/CoffeeMaker.java
@@ -2,17 +2,19 @@ package com.java.coffee;
 
 public class CoffeeMaker {
     private Boiler boiler;
+    private Filter filter;
 
     public CoffeeMaker() {
         boiler = new Boiler();
+        filter = new Filter();
     }
 
     public void putWaterGlasses(int numCups) {
         boiler.setCups(numCups);
     }
 
-    public void putCoffeeGrounds() {
-
+    public void putCoffeeGrounds(int quantity) {
+        filter.setCoffeeGrounds(quantity);
     }
 
     public void pressBrewButton() {
